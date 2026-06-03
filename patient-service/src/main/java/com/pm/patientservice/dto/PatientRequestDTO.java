8eb207128958fec3f2c7eb4beacec934c0f369d3
joy <joydeepdey648@gmail.com>
2026-06-03 16:13:11 +0530

    Patient service crud

---
+++ patient-service/src/main/java/com/pm/patientservice/dto/PatientRequestDTO.java
@@ -0,0 +1,65 @@
+package com.pm.patientservice.dto;
+
+import com.pm.patientservice.dto.validators.CreatePatientValidationGroup;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class PatientRequestDTO {
+    @NotBlank
+    @Size(max = 100, message = "Cannot exceed 100 chars")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Not valid email")
+    private String email;
+
+    @NotBlank
+    private String address;
+
+    @NotBlank
+    private String dob;
+
+    @NotBlank(groups = CreatePatientValidationGroup.class)
+    private String registeredDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getRegisteredDate() {
+        return registeredDate;
+    }
+
+    public void setRegisteredDate(String registeredDate) {
+        this.registeredDate = registeredDate;
+    }
+}
